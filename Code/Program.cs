6a74e737c173751bb4c8135a8593650dd91874fa
Ruben <ruben.pogosyan95@gmail.com>
2022-12-25 01:25:16 +0400

    Code

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "hello", "2", "world", ":))" };
+
+int new_size = 0;
+int max_lenght = 3;
+string[] temp_array = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= max_lenght)
+    {
+        temp_array[i] = array[i];
+        new_size++;
+    }
+}
+
+string[] new_array = new string[new_size];
+int new_index = 0;
+
+for (int i = 0; i < temp_array.Length; i++)
+{
+    if(temp_array[i] != null)
+    {
+        new_array[new_index] = temp_array[i];
+        new_index++;
+    }
+}
